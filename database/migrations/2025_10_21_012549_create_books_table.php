086b908f304546f0330708da6b71e7a30282b939
amanrai05 <rai877503@gmail.com>
2025-11-30 23:53:14 +0700

    Added Book model, migration, routes, and API setup

--- database/migrations/2025_10_21_012549_create_books_table.php
+++ database/migrations/2025_10_21_012549_create_books_table.php
@@ -6,22 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        // ========== 1 =========
-        // Create books table with necessary fields
-        // Fields: id, title, author, published_year, is_available, created_at, updated_at
         Schema::create('books', function (Blueprint $table) {
-
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->year('published_year');
+            $table->boolean('is_available')->default(1);
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('books');
